842cd285b5faa8538e4733febe6dba2b21948405
Darshan Tada <86282692+DarshanTada@users.noreply.github.com>
2024-05-26 20:07:33 -0700

    lab3 done

--- lab3/lab3.playground/Contents.swift
+++ lab3/lab3.playground/Contents.swift
@@ -147,12 +147,12 @@
 
 
 
-    // a. Usage of while loop to simulate adding students to the class until it is full.
+    // a. Usage of while loop to simulate adding students to the class until it is full
             currentNumberOfStudents = 0
             while currentNumberOfStudents < maxStudents {
                 addStudent(name: "New Student")
     
-    // b. Incrementing the number and printing the current number of students after each addition.
+    // b. Incrementing the number and printing the current number of students after each addition
                 print("Total number of students after addition are : \(currentNumberOfStudents)")
             }
 
@@ -165,8 +165,159 @@
 
 // Question 8 :-
 
+    
+    
+    // a. Use of repeat-while loop to simulate removing students from the class until it is down to the 5 original students
+        repeat {
+            studentNames.removeLast()
+            currentNumberOfStudents -= 1
+    // b. Decrementing the number and printing the current number of students after each removal.
+            print("Total number of students are \(currentNumberOfStudents)")
+        }  while currentNumberOfStudents > 5
+        
+    
+
+// Question 9 :-
+
+    
+
+        func checkStudentname(name: String) {
+            guard !name.isEmpty else {
+                print("\nError: Invalid student name!")
+                return
+            }
+            print("\nValid student name")
+        }
+        
+        checkStudentname(name: "")
+        checkStudentname(name: "Darshan")
 
 
 
 
+// Question 10 :-
+
+
+
+        func checkMiddleName(middleName: String?) {
+            
+            guard let enteredName = middleName  else {
+                    print("\nError: No middle name provided")
+                    return
+                }
+                print("\nMiddle name is \(enteredName)")
+        }
+
+        checkMiddleName(middleName: nil)
+        checkMiddleName(middleName: "dars")
+        
+        
+// or
+        
+        func checkMiddleName2(fullName: String) {
+            let splittedName = fullName.split(separator: " ")
+            guard splittedName.count == 3 else {
+                print("\nError: No middle name provided")
+                return
+            }
+            print("\nMiddle name of \(fullName) is \(splittedName[1])")
+        }
+        
+        checkMiddleName2(fullName: "")
+        checkMiddleName2(fullName: "Darshan keval darsh")
+
+
+
+
+// Question 11 :-
+
+
+
+            enum Grade {
+                case A
+                case B
+                case C
+                case D
+                case F
+            }
+            
+            func calculateGrade(averageGrade: Double) -> Grade {
+                switch averageGrade {
+                case 90...100 :
+                    return .A
+                case 80..<90 :
+                    return .B
+                case 70..<80 :
+                    return .C
+                case 60..<70 :
+                    return .D
+                default :
+                    return .F
+                }
+            }
+            
+            
+            func printGrade(averageScore: Double) {
+                let grade = calculateGrade(averageGrade: averageScore)
+                print("The grade for an average score of \(averageScore) is \(grade).")
+            }
+            
+            printGrade(averageScore: 92)
+            printGrade(averageScore: 83)
+            printGrade(averageScore: 72)
+            printGrade(averageScore: 61)
+            printGrade(averageScore: 59)
+         
+
+
+// Questions 12 :-
+
+
+
+
+        let congratulate: (String, String) -> Void = { (name, grade) in
+            if grade.lowercased() == "a" {
+                print("Congratulations \(name)! You received an A!")
+            }
+        }
+        
+        congratulate("Darshan", "A")
+        congratulate("Dars", "b")
+
+
+
+// Question 13 :-
+
+
+    // a and b is already defined from line number 236
+
+    // c
+        let students = [
+            ("Darshan", 92.0),
+            ("Keval", 85.0),
+            ("Sakshi", 74.5),
+            ("Nikhil", 65.0),
+            ("Yajurva", 55.0)
+        ]
+        
+        for (name, numericGrade) in students {
+            let grade = calculateGrade(averageGrade: numericGrade)
+            print("Student: \(name), Grade: \(grade)")
+        }
+
+
+
+// Question 14 :-
+
+
+    // a: Create an optional variable for the student's nickname
+        var studentNickname: String? = "Ally"
+        
+    // b.
+        if let nickname = studentNickname {
+            print("Nickname: \(nickname)")
+        } else {
+            print("No nickname")
+        }
+        
 
